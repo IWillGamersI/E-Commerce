7597d36908b4ef736a5b9a089e41f7e02c3d78ee
WillGamers1981 <gameloliniciante@gmail.com>
2021-07-22 18:22:38 -0300

    concluido

--- main.js
+++ main.js
@@ -1,32 +1,94 @@
 const items = [
     {
-        id:1,
-        nome: 'Produto 1',
+        id:0,
+        name: 'Produto 1',
         img:'img/placeholder.jpg',
-        quantidade: 0
+        amount: 0
+    },
+    {
+        id:1,
+        name: 'Produto 2',
+        img:'img/placeholder.jpg',
+        amount: 0
     },
     {
         id:2,
-        nome: 'Produto 2',
+        name: 'Produto 3',
         img:'img/placeholder.jpg',
-        quantidade: 0
-    },
-    {
-        id:3,
-        nome: 'Produto 3',
-        img:'img/placeholder.jpg',
-        quantidade: 0
+        amount: 0
     }
 ]
 
 bootShoop = () => {
+    
+    var itemsProduct = document.getElementById('produtos')
 
-    var itemsProduct = document.querySelector('#produtos')
+    items.map((value) => {
+        itemsProduct.innerHTML += `
 
-    items.map((value) =>{
-        console.log(value.nome)
-    })
+          <article class='product-single'>  
+            <img src='`+value.img+`'/>
+            <p>${value.name}</p>
+            <button id="add-car" key=${value.id} onClick="updateCar">Adicionar ao Carrinho</button>
+            
+          </article>
 
+        `
+    }) 
+     
 }
 
+
+
 bootShoop()
+
+
+
+updateInventory = () => {
+    
+    var itemsInventory = document.getElementById('inventory')
+    
+    itemsInventory.innerHTML = "";
+
+    items.map((value) => {
+        if(value.amount > 0){
+            itemsInventory.innerHTML += `
+
+            <article class="product-single">  
+                <p>    ID: ${value.id}</p>
+                <p>  Name: ${value.name}</p>
+                <p>Amount: ${value.amount}</p>
+                <hr>
+            </article>
+
+            `
+            }
+            
+    })
+    
+}
+
+var eachItems = document.getElementById('add-car')
+
+eachItems.addEventListener('click',updateCar)
+
+function updateCar (){
+    for(var i = 0; i < eachItems.length; i++){
+        if(eachItems[i] == items.id){
+            let key = this.getAttribute('key')
+            items[key].amount++
+        }
+         
+    }
+}
+
+
+
+
+
+
+
+
+
+console.log(items)
+console.log(eachItems)
